7efab253c623f7251733f7f7d13a7de0c6599740
Kevin <mrhallonline@gmail.com>
2019-11-01 15:31:03 -0500

    button click advance questionindex

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,14 @@ class _QuizPageState extends State<QuizPage> {
     
   ];
 
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  
+  int questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -41,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -66,12 +74,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                questionIndex=2;
+                  
                 });
               },
             ),
@@ -91,12 +95,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                  
                 });
               },
             ),
