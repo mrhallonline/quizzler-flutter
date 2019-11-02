bbd3a453b95b2fdc4fd3bc5559d0cb182670f78c
Kevin <mrhallonline@gmail.com>
2019-11-01 21:52:00 -0500

    modularizing widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -28,19 +31,7 @@ class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [
     
   ];
-
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
   
-  List<bool> answers = [
-    false,
-    true,
-    true
-  ];
-
   int questionNumber = 0;
 
   @override
@@ -55,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -79,7 +70,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   scoreKeeper.add(Icon(Icons.check,
                   color: Colors.green),);
@@ -89,7 +80,6 @@ class _QuizPageState extends State<QuizPage> {
                 }
                 setState(() {
                 questionNumber++;
-                  
                 });
               },
             ),
@@ -100,15 +90,14 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
               color: Colors.red,
-              child: Text(
-                'False',
+              child: Text('False',
                 style: TextStyle(
                   fontSize: 20.0,
                   color: Colors.white,
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == false){
                   scoreKeeper.add(Icon(Icons.check,
                   color: Colors.green),);
@@ -131,8 +120,3 @@ class _QuizPageState extends State<QuizPage> {
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
